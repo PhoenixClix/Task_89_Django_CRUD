be9de735eaab2248126a703c7f2f9a16cb7daa14
Mfirs <mfirstborn@gmail.comgit config --global user.name Mfirs  git config --global user.email mfirstborn@gmail.com>
2022-06-25 20:50:46 +0200

    view and templates added

--- I4G003259TSL/blog/views.py
+++ I4G003259TSL/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+  model = Post
+
+class PostCreateView(CreateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
+
+class DetailView():
+  class PostDetailView():
+    model = Post
+
+class PostUpdateView(UpdateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
